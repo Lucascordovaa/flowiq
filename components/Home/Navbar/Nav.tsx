c19b967a2ba3337b35fbdb7e14ff312602fe1123
Lucas Filipe Cordova <lucascordova1000@gmail.com>
2025-01-28 17:24:46 -0300

    first commit

---
+++ components/Home/Navbar/Nav.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Image from "next/image";
+import { navLinks } from "@/constant/Constant";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+const Nav = () => {
+  return (
+    <div className="z-[1000] h-[8.55vh] w-full bg-white transition-all duration-200">
+      <div className="mx-auto flex h-full w-[90%] items-center justify-between xl:w-3/5">
+        {/* logo */}
+        <Image src="/images/logo.PNG" alt="logo" width={150} height={150} />
+        {/* navigation links */}
+        <div className="hidden items-center space-x-10 lg:flex">
+          {navLinks.map((link) => {
+            return (
+              <Link href={link.url} key={link.id}>
+                <p className="nav_link">{link.label}</p>
+              </Link>
+            );
+          })}
+        </div>
+        {/* buttons */}
+        <div className="flex items-center space-x-6">
+          <Link href="/">
+            <p className="nav_link">Login</p>
+          </Link>
+          <Button className="bg-[#20a2a2] px-9 py-6 hover:bg-teal-200 hover:text-teal-700">
+            Sign Up
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
---
+++ constant/Constant.ts
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    id: 1,
+    url: "#",
+    label: "Pricing",
+  },
+  {
+    id: 2,
+    url: "#",
+    label: "Resources Center",
+  },
+  {
+    id: 3,
+    url: "#",
+    label: "About",
+  },
+  {
+    id: 4,
+    url: "#",
+    label: "Contact",
+  },
+];
